e48db271297cd74a9371f1317f36e8cfd556f686
Gregory N. Schmit <schmitgreg@gmail.com>
2019-07-16 22:44:04 -0500

    remove models since they were just for testing

--- autorest/models.py
+++
@@ -1,9 +0,0 @@
-# TODO: purely for dev testing, delete before pushing!
-
-
-from django.db import models
-
-class Thing(models.Model):
-    name = models.CharField(max_length=255)
-    is_cool = models.BooleanField(default=True)
-    desc = models.TextField(blank=True)
